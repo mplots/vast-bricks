4af97740773dbbbec66a1cc5fa24a561b99b4398
Maris Plots <maris.plots@gmail.com>
2025-05-30 21:16:22 +0300

    Analytics update

--- script.js
+++ script.js
@@ -1,3 +1,3 @@
 img=document.createElement('img')
-img.src="https://queue.simpleanalyticscdn.com/noscript.gif?hostname=splash.vastbricks.com&path="+window.location.pathname + window.location.search + window.location.hash.replace("#","");
+img.src="https://queue.simpleanalyticscdn.com/noscript.gif?hostname=splash.vastbricks.com&path="+window.location.pathname + window.location.search.replace("?", "") + window.location.hash.replace("#","");
 document.body.append(img)
